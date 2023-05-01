bf0230b14d9d5b57e04ea8a535f0a9fb56409060
ShaikMudsirAli1 <121999550+SHAIKADS@users.noreply.github.com>
2023-05-01 19:47:12 +0530

    Second Commit

--- travel.js
+++ travel.js
@@ -1,6 +1,5 @@
 const menuToggle = document.querySelector(".toggle");
 const showCase = document.querySelector(".showcase");
-
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showCase.classList.toggle("active");
